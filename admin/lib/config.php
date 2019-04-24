60974e734598744c466e09cba561bb5db88376ed
miovo <miovo@miovo.me>
2019-04-23 21:49:40 -0400

    Rollout test admin page

---
+++ admin/lib/config.php
@@ -0,0 +1,14 @@
+<?php
+// ERROR REPORTING
+error_reporting(E_ALL & ~E_NOTICE);
+
+// DATABASE SETTINGS
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cnScreen');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'miovo');
+define('DB_PASSWORD', '@ColeM783');
+
+// FILE PATHS
+define("PATH_LIB", __DIR__ . DIRECTORY_SEPARATOR);
+?>
--- assets/configs/config.php
+++ assets/configs/config.php
@@ -2,7 +2,7 @@
 
 	//Basic Info
 	$locationName = "Hamilton Mill";
-	$kioskURL = "https://google.com";
+	$kioskURL = "https://codeninjas.com";
 
 	//Social
 	$Facebook = "";
@@ -10,5 +10,18 @@
 
 	//Styles
 	$SidebarBGColor = "#545b62";
+
+	//Sensei
+	$sensei1 = "1";
+	$sensei2 = "2";
+	$sensei3 = "3";
+	$sensei4 = "4";
+	$sensei5 = "5";
+	$sensei6 = "6";
+	$sensei7 = "7";
+	$sensei8 = "8";
+	$sensei9 = "9";
+	$sensei10 = "10";
+
 
 ?>
