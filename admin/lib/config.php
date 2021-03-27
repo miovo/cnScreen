2bc59d7373c7d364568b9b9911b41966a172a130
miovo <miovo@miovo.me>
2021-03-26 20:29:41 -0400

    Update config.php

--- admin/lib/config.php
+++ admin/lib/config.php
@@ -4,10 +4,10 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 // DATABASE SETTINGS
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'cnScreen');
+define('DB_NAME', '');
 define('DB_CHARSET', 'utf8');
-define('DB_USER', 'miovo');
-define('DB_PASSWORD', '@ColeM783');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
 
 // FILE PATHS
 define("PATH_LIB", __DIR__ . DIRECTORY_SEPARATOR);
